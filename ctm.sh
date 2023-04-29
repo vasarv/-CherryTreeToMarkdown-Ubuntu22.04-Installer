0549cd4f78883d7527d5b29105f677537541edee
VasarV <76836836+vasarv@users.noreply.github.com>
2023-04-29 14:59:58 +0000

    Update ctm.sh

--- ctm.sh
+++ ctm.sh
@@ -1,12 +1,12 @@
-#https://gitlab.com/kibley/cherrytreetomarkdown
-
-sudo apt update
-sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common git -y
-sudo add-apt-repository ppa:ondrej/php
-sudo apt update
-sudo apt install php8.0 php8.0-mbstring php8.0-xml php8.0-exif -y
-git clone https://gitlab.com/kibley/cherrytreetomarkdown.git
-cd cherrytreetomarkdown/
-clear
-php composer.phar install
-echo "CherryTreeToMarkdown installed!!!"
+sudo apt update &&
+sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common git -y &&
+sudo add-apt-repository ppa:ondrej/php &&
+sudo apt update &&
+sudo apt install php8.0 php8.0-mbstring php8.0-xml php8.0-exif -y &&
+git clone https://gitlab.com/kibley/cherrytreetomarkdown.git &&
+cd cherrytreetomarkdown/ &&
+clear &&
+php composer.phar install &&
+echo "CherryTreeToMarkdown installed!!!" &&
+echo "---------------------------------" &&
+php cherrytomd.php --help &&
